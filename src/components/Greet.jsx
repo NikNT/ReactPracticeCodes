52317e27686b94b32b60ee43c5711f254546458c
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2021-11-17 18:39:51 -0500

    Initial Commit - Hello World, Folder Str, Comp Types

---
+++ src/components/Greet.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// function Greet() {
+//   return <h1>Hello, Nikhil!</h1>;
+// }
+
+const Greet = () => <h1>Hello, Nikhil!!</h1>;
+
+export default Greet;
+
+//Named -> We have to consider the very same function
+//Import as {Greet} from '../location'
+
+//For default, we can specify any names
